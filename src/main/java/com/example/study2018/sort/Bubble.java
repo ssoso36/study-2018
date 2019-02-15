d1022e6605b2df83abad38c6ad87c452d104e2c0
김소연 <soyeon_k@encar.com>
2019-02-15 17:42:00 +0900

    array sort

---
+++ src/main/java/com/example/study2018/sort/Bubble.java
@@ -0,0 +1,8 @@
+package com.example.study2018.sort;
+
+public class Bubble {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
---
+++ src/main/java/com/example/study2018/sort/Selection.java
@@ -0,0 +1,15 @@
+package com.example.study2018.sort;
+
+public class Selection {
+
+    public static void main(String[] args) {
+
+        int arr[] = {29, 10, 14, 37, 13};
+
+        // 가장 최대값을 찾는다.
+
+        // 최대값은 맨 오른쪽에 넣는다.
+
+        //
+    }
+}
